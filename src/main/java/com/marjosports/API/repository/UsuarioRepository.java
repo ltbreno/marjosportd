47651d532c06f180f9269dd313b9b06c1d79dde4
Júlio César Alexandrino Costa <juliocesarcac@academicos.com>
2024-06-29 18:44:23 -0300

    cpfs

--- src/main/java/com/marjosports/API/repository/UsuarioRepository.java
+++ src/main/java/com/marjosports/API/repository/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package com.marjosports.API.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.marjosports.API.model.Usuario;
