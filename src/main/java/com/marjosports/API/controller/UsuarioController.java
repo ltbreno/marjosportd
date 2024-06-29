3c12674ea2320cdb7eea7fbeb4d5039f1ade5971
Júlio César Alexandrino Costa <juliocesarcac@academicos.com>
2024-06-29 18:54:12 -0300

    get

--- src/main/java/com/marjosports/API/controller/UsuarioController.java
+++ src/main/java/com/marjosports/API/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +19,17 @@ import com.marjosports.API.service.UsuarioService;
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
-	@Autowired
-	UsuarioService usuarioService;
-	
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public void create(@RequestBody Usuario usuario) {
-		usuarioService.create(usuario);
-	}
-	
-	@PostMapping("/login")
+    
+    @Autowired
+    UsuarioService usuarioService;
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Usuario usuario) {
+        usuarioService.create(usuario);
+    }
+    
+    @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody Usuario usuario) {
         Usuario loggedUser = usuarioService.login(usuario.getCpf(), usuario.getPassword());
         
@@ -37,16 +39,21 @@ public class UsuarioController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed. Please check your credentials.");
         }
     }
-	
-	  @GetMapping("/cpfs")
-	    public ResponseEntity<?> getAllCpfs(@RequestBody Usuario usuario) {
-	        if (usuario.isAdmin()) {
-	            List<String> allCpfs = usuarioService.getAllCpfs();
-	            return ResponseEntity.ok(allCpfs);
-	        } else {
-	            String cpf = usuario.getCpf(); 
-	            return ResponseEntity.ok(cpf);
-	        }
-	    }
-
+    
+    @GetMapping("/cpfs/{id}")
+    public ResponseEntity<?> getAllCpfs(@PathVariable Long id) {
+        Usuario usuario = usuarioService.findById(id);
+        
+        if (usuario == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        
+        if (usuario.isAdmin()) {
+            List<String> allCpfs = usuarioService.getAllCpfs();
+            return ResponseEntity.ok(allCpfs);
+        } else {
+            String cpf = usuario.getCpf(); 
+            return ResponseEntity.ok(cpf);
+        }
+    }
 }
